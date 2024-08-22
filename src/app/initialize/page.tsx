b149e4cfccc74b2b752bcc0d2abcf9c80d422c9d
Evan Gary <evanmgary@gmail.com>
2024-08-22 16:04:15 -0500

    remove init statement causing error

--- src/app/initialize/page.tsx
+++ src/app/initialize/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import prisma from "../client";
 import { initializeDB } from "../actions/initActions"
 
 export default function Page(){
